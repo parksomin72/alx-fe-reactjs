0f2cc6c33c498543eb0535cbc20ba49e066aa568
parksomin72 <fatimazahraealawi@gmail.com>
2025-07-13 09:01:31 -0700

    .

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,6 +3,7 @@ function WelcomeMessage() {
         <div>
             <h1>Welcome to ALX React APP!</h1>
             <p>This is a simple JSX component.</p>
+	    <p>I am learning about JSX!</p>
 	    <p>Hello everyone, I am learning React at ALX!</p>
         </div>
     );
