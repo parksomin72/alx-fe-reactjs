e6c3804f2e769ff56f7405a76129a97d31a1e9fe
parksomin72 <fatimazahraealawi@gmail.com>
2025-08-11 12:05:17 -0700

    add file

---
+++ alx-react-app-props/src/components/WelcomeMessage.jsx
@@ -0,0 +1,12 @@
+function WelcomeMessage() {
+    return (
+        <div>
+            <h1>Welcome to ALX React APP!</h1>
+            <p>This is a simple JSX component.</p>
+	    <p>I am learning about JSX!</p>
+	    <p>Hello everyone, I am learning React at ALX!</p>
+        </div>
+    );
+}
+
+export default WelcomeMessage;
